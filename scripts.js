af277336a36871519f140bf45d6bdb3375805469
TomE <tommevans210@gmail.com>
2022-12-09 10:06:04 +0700

    It's done. styles need some work but it''s done.

--- scripts.js
+++ scripts.js
@@ -7,5 +7,18 @@ function palindrome() {
   const word = document.querySelector('.input-text').value;
   let len = word.length;
 
-  let start = word.substring(0);
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+  console.log(end);
+  console.log(start);
+
+  // let flip = end.split('').reverse().join('');
+
+  let flip = [...end].reverse().join('');
+  if (start == flip) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
+  } else {
+    result.innerHTML = `${word.toUpperCase()} is Not a palindrome`;
+  }
+  console.log(flip);
 }
